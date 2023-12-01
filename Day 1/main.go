cbde43013bd120b4dc10204fd012c293f0c1305d
tomprogramuje <tom.severa@outlook.cz>
2023-12-01 13:46:17 +0100

    day 1 exc 2

--- Day 1/main.go
+++ Day 1/main.go
@@ -6,26 +6,71 @@ import (
 	"io"
 	"os"
 	"regexp"
+	"sort"
 	"strconv"
 )
 
+type Number int
+
+const (
+	zero Number = iota
+	one
+	two
+	three
+	four
+	five
+	six
+	seven
+	eight
+	nine
+)
+
+func (n Number) String() string {
+	return [...]string{"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"}[n]
+}
+
 func sumNumbersFromLine(rd bufio.Reader) int {
 	fileSum := 0
+
 	for {
 		line, err := rd.ReadString('\n')
 		if err == io.EOF {
-			return fileSum
+			break
 		}
 		if err != nil {
 			fmt.Println("Error:", err)
 		}
-	
+
+		occurences := make(map[int]int)
+
+		for i := zero; i <= nine; i++ {
+			numWord := Number.String(i)
+			re := regexp.MustCompile(numWord)
+			indices := re.FindAllStringIndex(line, -1)
+			for _, index := range indices {
+				startIndex := index[0]
+				occurences[startIndex] = int(i)
+			}
+		}
+
 		re := regexp.MustCompile("[0-9]")
-		nums := re.FindAllString(line, -1)	
-		firstNum := nums[0]
-		secondNum := nums[len(nums)-1]
-		
-		calibValue := firstNum + secondNum
+		indices := re.FindAllStringIndex(line, -1)
+		for _, index := range indices {
+			startIndex := index[0]
+			expression := line[startIndex : startIndex+1]
+			number, err := strconv.Atoi(expression)
+			if err == nil {
+				occurences[startIndex] = number
+			}
+		}
+
+		keys := make([]int, 0, len(occurences))
+		for k := range occurences {
+			keys = append(keys, k)
+		}
+		sort.Ints(keys)
+
+		calibValue := strconv.Itoa(occurences[keys[0]]) + strconv.Itoa(occurences[keys[len(keys)-1]])
 		calibrationValue, err := strconv.Atoi(calibValue)
 		if err != nil {
 			fmt.Println(err)
