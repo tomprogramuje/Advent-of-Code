b6a902854eb37d9d98d386c4046eba80e9284deb
tomprogramuje <tom.severa@outlook.cz>
2023-12-02 14:14:35 +0100

    day 2 exc 2

--- 2023/Day 2/main.go
+++ 2023/Day 2/main.go
@@ -6,14 +6,17 @@ import (
 	"io"
 	"os"
 	"regexp"
+	"sort"
 	"strconv"
+	"strings"
 )
 
-func sumOfIDs(rd bufio.Reader) int {
+func sumOfIDs(rd bufio.Reader) (int, int) {
 	idSum := 0
+	powerSum := 0
 
 	for {
-		line, err := rd.ReadString('\n')
+		line, _, err := rd.ReadLine()
 		if err == io.EOF {
 			break
 		}
@@ -28,24 +31,66 @@ func sumOfIDs(rd bufio.Reader) int {
 		}
 		notAllowed := true
 		for _, pattern := range patterns {
-			if pattern.MatchString(line) {
+			if pattern.MatchString(string(line)) {
 				notAllowed = false
 				break
 			}
 		}
 		if notAllowed {
-			fmt.Println("valid game")
 			gameID := regexp.MustCompile(`Game (\d+):`)
-			match := gameID.FindStringSubmatch(line)
-			fmt.Println(match[1])
+			match := gameID.FindStringSubmatch(string(line))
 			value, err := strconv.Atoi(match[1])
 			if err != nil {
 				fmt.Println(err)
 			}
 			idSum += value
 		}
+		red := regexp.MustCompile(`\b(\d+) red\b`)
+		allRed := red.FindAllStringSubmatch(string(line), -1)
+		green := regexp.MustCompile(`\b(\d+) green\b`)
+		allGreen := green.FindAllStringSubmatch(string(line), -1)
+		blue := regexp.MustCompile(`\b(\d+) blue\b`)
+		allBlue := blue.FindAllStringSubmatch(string(line), -1)
+
+		var allRedCubes, allGreenCubes, allBlueCubes []int
+		powerGame := 1	
+
+		for i := range allRed {
+			num := strings.Split(allRed[i][0], " ")
+			numParsed, err := strconv.Atoi(num[0])
+			if err != nil {
+				fmt.Println(err)
+			}
+			allRedCubes = append(allRedCubes, numParsed)
+		}
+		sort.Ints(allRedCubes)
+		powerGame *= allRedCubes[len(allRedCubes)-1]
+
+		for i := range allGreen {
+			num := strings.Split(allGreen[i][0], " ")
+			numParsed, err := strconv.Atoi(num[0])
+			if err != nil {
+				fmt.Println(err)
+			}
+			allGreenCubes = append(allGreenCubes, numParsed)
+		}
+		sort.Ints(allGreenCubes)
+		powerGame *= allGreenCubes[len(allGreenCubes)-1]
+
+		for i := range allBlue {
+			num := strings.Split(allBlue[i][0], " ")
+			numParsed, err := strconv.Atoi(num[0])
+			if err != nil {
+				fmt.Println(err)
+			}
+			allBlueCubes = append(allBlueCubes, numParsed)
+		}
+		sort.Ints(allBlueCubes)
+		powerGame *= allBlueCubes[len(allBlueCubes)-1]
+
+		powerSum += powerGame
 	}
-	return idSum
+	return idSum, powerSum
 }
 
 func main() {
